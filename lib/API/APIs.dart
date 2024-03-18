3de19a832a745ac9d6f2b4056cb3fc3e2055fb1c
Devan Bhensdadiya <92723752+devanpatel28@users.noreply.github.com>
2024-03-18 23:44:13 +0530

    Property Document DONE

--- lib/API/APIs.dart
+++ lib/API/APIs.dart
@@ -1,5 +1,5 @@
 // String Host = "http://localhost:3000";
-String Host = "https://3c58-2409-40c1-3b-c4dd-349e-1313-80a6-33c7.ngrok-free.app";
+String Host = "https://breakable-shorts-ox.cyclic.app";
 
 // Users
 String validateuserAPI = "$Host/user/validateuser";
